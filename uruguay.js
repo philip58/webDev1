c0d6974b0c68597db154be43a00bc9566be279a0
Philip Fernandez <philipfernandezz@gmail.com>
2022-09-26 19:03:45 -0400

    Uruguay.html/css/js done

---
+++ uruguay.js
@@ -0,0 +1,12 @@
+const changecolor2 = () =>{
+    if(document.body.style.color === 'black'){
+        document.body.style.color = 'white';
+    }
+    else if(document.body.style.color === 'white'){
+        document.body.style.color = 'blue';
+    }
+    else{
+        document.body.style.color = 'black';
+    }
+
+}
